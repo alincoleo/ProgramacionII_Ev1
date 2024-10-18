cc27a5b28181985ee729790782e99399a2137dee
alincoleo <alvaro.lincoleo@gmail.com>
2024-10-18 19:20:04 -0300

    feature v1.1.0: se crean metodo que permite modificar todas las variables de precio, se agrega formato, las variables que se repiten se instancian en el contexto de la clase principal de forma privada y nula para luego ser usada y modifica por los metodos internos

--- app/src/main/java/cl/alincoleo/android/ensayoprueba/MainActivity.kt
+++ app/src/main/java/cl/alincoleo/android/ensayoprueba/MainActivity.kt
@@ -4,19 +4,35 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.widget.EditText
+import android.widget.Switch
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import cl.alincoleo.android.ensayoprueba.modelo.CuentaMesa
 import cl.alincoleo.android.ensayoprueba.modelo.ItemMenu
 import cl.alincoleo.android.ensayoprueba.modelo.ItemMesa
+import java.text.NumberFormat
+import java.util.Locale
+import kotlin.math.roundToInt
 
 
 class MainActivity : AppCompatActivity() {
-    //objetos del menu
-    val ramen = ItemMenu("Ramen",6000)
-    val yakisoba = ItemMenu("Yakisoba",8000)
+    // objetos en private para que solo existan en esta clase
+    private val ramen = ItemMenu("Ramen",6000)
+    private val yakisoba = ItemMenu("Yakisoba",8000)
+    private val mesaActual = CuentaMesa(1)
+    //aquí creamos los accesos a la aplicaccion tal de no necesitar crearlas constantemente
+    private var etCanRamen: EditText? = null
+    private var etCanYakisoba: EditText? = null
+    private var swPropina : Switch? = null
+    //aquí creamos los objetos de los textos a modificar
+    private var tvTotalRamen : TextView? = null
+    private var tvTotalYakisoba : TextView? = null
+    private var tvTotalSinPropina : TextView? = null
+    private var tvPropina : TextView? = null
+    private var tvTotalAPagar : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,8 +43,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val etCanRamen = findViewById<EditText>(R.id.etCanRamen)
-
+        //aquí asignamos los botones de la pantalla
+        etCanRamen = findViewById<EditText>(R.id.etCanRamen)
+        etCanYakisoba = findViewById<EditText>(R.id.etCanYakisoba)
+        swPropina = findViewById<Switch>(R.id.switchPropina)
+        //aquí los textos a editar
+        tvTotalRamen = findViewById<TextView>(R.id.tvTotalRamen)
+        tvTotalYakisoba = findViewById<TextView>(R.id.tvTotalYakisoba)
+        tvTotalSinPropina = findViewById<TextView>(R.id.tvTotalSinPropina)
+        tvPropina = findViewById<TextView>(R.id.tvPropina)
+        tvTotalAPagar = findViewById<TextView>(R.id.tvTotalApagar)
 
         val textWatcher: TextWatcher = object:TextWatcher{
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
@@ -40,17 +64,39 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun afterTextChanged(p0: Editable?) {
-                //enviar total ramen
-                calcularSubtotalRamen()
+                //enviar a calcular total
+                calcularTotales()
             }
         }
-        etCanRamen.addTextChangedListener(textWatcher)
+        etCanRamen?.addTextChangedListener(textWatcher)
+        etCanYakisoba?.addTextChangedListener(textWatcher)
+        swPropina?.setOnCheckedChangeListener { compoundButton, b -> calcularTotales() }
     }
-    fun calcularSubtotalRamen(){
-        val etCanRamen = findViewById<EditText>(R.id.etCanRamen)
-        val cantidadnumeroRamen = etCanRamen.text.toString().toIntOrNull()?:0
-        val itemActual = ItemMesa(ramen,cantidadnumeroRamen)
+    fun calcularTotales(){
+        //si no limpio la lista empieza a llenarse de nuevo
+        mesaActual.items.clear()
+        val cantidadnumeroRamen = etCanRamen?.text.toString().toIntOrNull()?:0
+        val itemRamen = ItemMesa(ramen,cantidadnumeroRamen)
+        //objeto que permite formatear, se establecen las monedas de chile, sin decimales
+        val formatoMoneda= NumberFormat.getCurrencyInstance(Locale("es","CL")).apply { maximumFractionDigits=0 }
         //tvTotalRamen
-        findViewById<TextView>(R.id.tvTotalRamen).text= itemActual.calcularSubtotal().toString()
+        tvTotalRamen?.text= formatoMoneda.format(itemRamen.calcularSubtotal())
+
+        val cantidadnumeroYakisoba = etCanYakisoba?.text.toString().toIntOrNull()?:0
+        val itemYakisoba = ItemMesa(yakisoba,cantidadnumeroYakisoba)
+        tvTotalYakisoba?.text= formatoMoneda.format(itemYakisoba.calcularSubtotal())
+
+        mesaActual.agregarItem(itemRamen)
+        mesaActual.agregarItem(itemYakisoba)
+        val valorTotalSinPropina = mesaActual.calcularTotalSinPropina()
+        var propina = 0
+        var valorTotalConPropina = valorTotalSinPropina
+        tvTotalSinPropina?.text= "Comida ${formatoMoneda.format(valorTotalSinPropina)}"
+        if(swPropina?.isChecked == true){
+            propina = mesaActual.calcularPropina().roundToInt()
+            valorTotalConPropina= mesaActual.calcularTotalConPropina().roundToInt()
+        }
+        tvPropina?.text= "Propina ${formatoMoneda.format(propina)}"
+        tvTotalAPagar?.text= "Total ${formatoMoneda.format(valorTotalConPropina)}"
     }
 }
